aef8529ea95255a5444e418ee8e8fb0251191f28
Mohamedsabry-2000 <sabrry30@gmail.com>
2026-07-28 11:55:26 -0700

    Add files via upload
    نسخة 2

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE_NAME = 'ms-tasks-v1';
-const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+const CACHE_NAME = 'ms-tasks-v2';
+const ASSETS = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo-header.png'];
 
 self.addEventListener('install', (e) => {
   self.skipWaiting();
