321ee17a603b43f3518f80beed9f653415a2dec9
Mohamedsabry-2000 <sabrry30@gmail.com>
2026-07-31 12:50:07 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'ms-tasks-v19';
+const CACHE_NAME = 'ms-tasks-v20';
 const ASSETS = [
   './index.html', './manifest.json', './icon-192.png', './icon-512.png', './logo-header.png',
   'https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js',
